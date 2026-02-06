2dfe29f8227ae7247d5f8c7f6ca798b219e70a06
ANTONIO <antojoseabad@gmail.com>
2026-02-06 21:52:11 +0100

    layout component and route

---
+++ src/app/country/layouts/CountryLayout/CountryLayout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-country-layout',
+  imports: [RouterOutlet],
+  templateUrl: './CountryLayout.html',
+})
+export class CountryLayout {
+  
+}
